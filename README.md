ef7530e1c068cb459a91f7e25df23cb8e3cd2351
Julio Lopez <ljuliom@gmail.com>
2015-06-25 00:52:46 -0500

    persistency with the apikey

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ted-cli
 Command Line Interface gem for explore and watch Ted talks
+
+This application has been developed using the TED API and is not an official service of TED
+
+## Configure your API Key
+
+ted-cli key YOUR-API-KEY
+
+## Search Talks
+
+## Search by Speaker
+
+## Surprise me
+Do you want to see a random talk of your interest? Check it out!
+
+## Download Talk
--- lib/ted_cli.rb
+++ lib/ted_cli.rb
@@ -1,6 +1,18 @@
 require 'httparty'
+require 'yaml'
 
 require "ted-cli/version"
+require "ted-cli/talk"
 
 module TedCli
+  def self.api_key=(key)
+    y = YAML::load_file('conf/conf.yml')
+    y['content']['apikey'] = key
+    File.open('conf/conf.yml', 'w') {|f| f.write y.to_yaml }
+  end
+
+  def self.api_key
+    y = YAML::load_file('conf/conf.yml')
+    y['content']['apikey']
+  end
 end
