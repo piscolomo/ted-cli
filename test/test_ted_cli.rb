386068fc9808a3e849fdb27cca378b6cce549bb0
Julio Lopez <ljuliom@gmail.com>
2015-06-25 18:31:54 -0500

    just simple test for configure the api key

--- test/test_ted_cli.rb
+++ test/test_ted_cli.rb
@@ -1,3 +1,8 @@
-test "testing..." do
-  assert_equal 1, 1
+require File.expand_path("../lib/ted_cli", File.dirname(__FILE__))
+
+scope do
+  test "get false if apikey is not configured" do
+    TedCli.api_key = "mi-api-key"
+    assert_equal TedCli.api_key, "mi-api-key"
+  end
 end
