2c3891ed5c1c6e57237b0974cf3cae158ba4865c
Julio Lopez <ljuliom@gmail.com>
2015-06-25 02:55:56 -0500

    creating the yaml in the gempath

--- lib/ted_cli.rb
+++ lib/ted_cli.rb
@@ -5,13 +5,15 @@ require "ted-cli/version"
 require "ted-cli/talk"
 
 module TedCli
+  GEM_PATH = Gem::Specification.find_by_name("ted-cli").gem_dir
+  
   def self.api_key=(key)
     data = {"apikey" => key}
-    File.open("conf.yml", "w+") {|f| f.write(data.to_yaml) }
+    File.open(GEM_PATH + "/conf.yml", "w+") {|f| f.write(data.to_yaml) }
   end
 
   def self.api_key
-    y = YAML::load_file('conf.yml')
+    y = YAML::load_file(GEM_PATH + '/conf.yml')
     y['apikey']
   rescue
     false
